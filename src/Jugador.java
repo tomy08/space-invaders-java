f48aabb2b214454565ee45cf53032b64c0227643
tomy08 <tomasesantacruz@gmail.com>
2025-08-27 17:59:44 -0300

    Add jugador class

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,4 +1,33 @@
+import java.awt.*;
 
 public class Jugador {
+	public int x, y;
+	public int ancho = 40, alto = 20;
+	public int velocidad = 5;
 
+	public Jugador(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void moverIzquierda() {
+		x -= velocidad;
+		if (x < 0)
+			x = 0;
+	}
+
+	public void moverDerecha() {
+		x += velocidad;
+		if (x + ancho > PanelJuego.ANCHO)
+			x = PanelJuego.ANCHO - ancho;
+	}
+
+	public int getCentroX() {
+		return x + ancho / 2;
+	}
+
+	public void dibujar(Graphics g) {
+		g.setColor(Color.CYAN);
+		g.fillRect(x, y, ancho, alto);
+	}
 }
