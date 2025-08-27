0ca47a8234430c2dc4b695028289f7d4b1e849c2
tomy08 <tomasesantacruz@gmail.com>
2025-08-27 17:59:10 -0300

    Add enemigo class

--- src/Enemigo.java
+++ src/Enemigo.java
@@ -1,4 +1,30 @@
+import java.awt.*;
 
 public class Enemigo {
+	public int x, y;
+	public int ancho = 30, alto = 20;
+	private int velocidad = 2;
+	private int direccion = 1; // 1 derecha, -1 izquierda
 
+	public Enemigo(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void mover() {
+		x += velocidad * direccion;
+		if (x <= 0 || x + ancho >= PanelJuego.ANCHO) {
+			direccion *= -1;
+			y += 20; // bajan un poco al rebotar
+		}
+	}
+
+	public void dibujar(Graphics g) {
+		g.setColor(Color.GREEN);
+		g.fillRect(x, y, ancho, alto);
+	}
+
+	public Rectangle getRect() {
+		return new Rectangle(x, y, ancho, alto);
+	}
 }
